480ce2c331b6ba75f39475c44006f25fa90eedc7
Matej Kralovic <matejkralovic77@gmail.com>
2026-05-21 21:08:59 +0200

    Navigation

--- app/src/main/java/me/matejkralovic/diacur/MainActivity.kt
+++ app/src/main/java/me/matejkralovic/diacur/MainActivity.kt
@@ -11,15 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import me.matejkralovic.diacur.ui.navigation.DiaCurNavGraph
 import me.matejkralovic.diacur.ui.theme.DiaCurTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val navController = rememberNavController()
-        DiaCurNavGraph(navController = navController)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+            DiaCurNavGraph(navController = navController)
             DiaCurTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
